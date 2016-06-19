319e6886620a46fe93118d99be4610eb3c4e1183
Arnold Lei <arnold.lei88@gmail.com>
2016-06-19 17:28:26 -0400

    working on countdown part of the schedule.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # train_schedule
+This is my train schedule game. I am spending less time on this project than I would like. I want to make sure that my group project is the focus. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,38 @@
 $('document').ready(function(){
 	var train = new Firebase("https://arnoldlei-train.firebaseio.com/");
+
+	function clock(){
+		var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+		$('.clock').html('<h2>'+time+'</h2>');
+		setTimeout(clock, 1000);
+	}
+
+	clock();
 	    
 	// $('#frequency').datepicker({
 	//     format: "hh:mm:ss"
 	// });  
 
 	$('#submit').on('click', function(){
-		console.log('something');
+		//the frequency and next arrival must be times
 		trainName = $('#trainName').val().trim();
 	  	destination = $('#destination').val().trim();
 	  	nextArrival = $('#nextArrival').val().trim();
-	  	frequency = $('#frequency').val().trim();
+	  	if(number($('#frequency').val().trim()) > number(nextArrival)){
+	  		frequency = $('#frequency').val().trim();
+	  	} else {
+	  		alert('the frequency must be greater than the next arrival');
+	  		return;
+	  	}
+	  	var minutesAway = number(nextArrival) - number(frequency);
 		
 		train.push({
 			trainName: trainName,
 			destination: destination,
 			nextArrival: nextArrival,
 			frequency: frequency,
-			// dateAdded: Firebase.ServiceValue.TIMESTAMP,
+			minutesAway: minutesAway,
+			dateAdded: Firebase.ServiceValue.TIMESTAMP,
 		});
 
 		//resets the value fields of the submit form 
@@ -32,12 +47,13 @@ $('document').ready(function(){
 	    tDestination = $('<td>'+ child.val().destination+'</td>');
 	    tNextArrival = $('<td>'+ child.val().nextArrival+'</td>');
 	    tFrequency = $('<td>'+ child.val().frequency+'</td>');
-	    tRate = $('<td>'+ child.val().frequency+'</td>');
-	    row.append(tTrainName, tDestination, tNextArrival, tFrequency, tRate);
+	    tMinutesAway = $('<td>'+ child.val().minutesAway+'</td>');
+	    row.append(tTrainName, tDestination, tNextArrival, tFrequency, tMinutesAway);
 	    $('tbody').append(row)
-
 	});
 
-
+    // $('#frequency').datepicker({
+    //     format: "dd/mm/yyyy"
+    // });
 	
 });
