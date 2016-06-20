f62defb1e82b5cf2d2d66d7a2be07576ea240ae2
Arnold Lei <arnold.lei88@gmail.com>
2016-06-19 23:43:44 -0400

    testing and learning moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,17 @@ $('document').ready(function(){
 	var train = new Firebase("https://arnoldlei-train.firebaseio.com/");
 
 	function clock(){
+
+		var minutes = 90;
+		var now = moment();
+		var future = moment().add(minutes, 'minutes');
 		var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+		var test = moment.duration(future.diff(now)).asMinutes();
+
 		$('.clock').html('<h2>'+time+'</h2>');
+		$('.test').html('<h2>'+future+'</h2>');
+		$('.test2').html('<h2>'+test+'</h2>');
 		setTimeout(clock, 1000);
 	}
 
@@ -13,44 +22,38 @@ $('document').ready(function(){
 	//     format: "hh:mm:ss"
 	// });  
 
-	$('#submit').on('click', function(){
+	$('#submit').on('click', function(event){
 		//the frequency and next arrival must be times
 		trainName = $('#trainName').val().trim();
 	  	destination = $('#destination').val().trim();
-	  	nextArrival = $('#nextArrival').val().trim();
-	  	if(number($('#frequency').val().trim()) > number(nextArrival)){
-	  		frequency = $('#frequency').val().trim();
-	  	} else {
-	  		alert('the frequency must be greater than the next arrival');
-	  		return;
-	  	}
-	  	var minutesAway = number(nextArrival) - number(frequency);
+	  	nextArrival = ($('#nextArrival').val().trim());
+	  	frequency = $('#frequency').val().trim();
+	  	var minutesAway = number(frequency) - number(nextArrival);
 		
 		train.push({
 			trainName: trainName,
 			destination: destination,
-			nextArrival: nextArrival,
+			nextArrival: minutesAway,
 			frequency: frequency,
-			minutesAway: minutesAway,
-			dateAdded: Firebase.ServiceValue.TIMESTAMP,
+			now: moment(),
 		});
 
 		//resets the value fields of the submit form 
 		$('input').val('');
-
+		event.preventDefault();
 		return false;
 	});
 
-	train.on('child_added', function(child, prevChild){
-		row = $('<tr>');
-	    tTrainName = $('<td>'+ child.val().trainName +'</td>');
-	    tDestination = $('<td>'+ child.val().destination+'</td>');
-	    tNextArrival = $('<td>'+ child.val().nextArrival+'</td>');
-	    tFrequency = $('<td>'+ child.val().frequency+'</td>');
-	    tMinutesAway = $('<td>'+ child.val().minutesAway+'</td>');
-	    row.append(tTrainName, tDestination, tNextArrival, tFrequency, tMinutesAway);
-	    $('tbody').append(row)
-	});
+	// train.on('child_added', function(child, prevChild){
+	// 	row = $('<tr>');
+	//     tTrainName = $('<td>'+ child.val().trainName +'</td>');
+	//     tDestination = $('<td>'+ child.val().destination+'</td>');
+	//     tNextArrival = $('<td>'+child.val().nextArrival +'</td>');
+	//     tFrequency = $('<td>'+ child.val().frequency+'</td>');
+	//     tMinutesAway = $('<td>'+ child.val().minutesAway+'</td>');
+	//     row.append(tTrainName, tDestination, tNextArrival, tFrequency, tMinutesAway);
+	//     $('tbody').append(row)
+	// });
 
     // $('#frequency').datepicker({
     //     format: "dd/mm/yyyy"
